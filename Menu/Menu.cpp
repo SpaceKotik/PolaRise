0562c276fadbdb7f4ca6b7d2ce7abfea18ab0ba3
Spacekotik <speedcuber17@yandex.ru>
2020-05-07 03:27:15 +0300

    WIP Adding menu

---
+++ Menu/Menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ilya on 5/7/20.
+//
+
+#include "Menu.h"
---
+++ Menu/Menu.h
@@ -0,0 +1,14 @@
+//
+// Created by ilya on 5/7/20.
+//
+
+#ifndef POLARISE_MENU_H
+#define POLARISE_MENU_H
+
+
+class Menu {
+
+};
+
+
+#endif //POLARISE_MENU_H
